b06fd9843452cabdc7ce013382d1ed8cd9e62ccc
franclobo <fjbl2788@gmail.com>
2022-05-17 20:07:20 -0500

    Fix linters

--- calculator.js
+++ calculator.js
@@ -4,20 +4,32 @@ class Calculator {
     this.num2 = num2;
   }
 
-  add (num1, num2) {
-    return num1 + num2;
+  add(num1, num2) {
+    num1 = this.num1;
+    num2 = this.num2;
+    let sum = num1 + num2;
+    return sum;
   }
 
   subtract(num1, num2) {
-    return num1 - num2;
+    num1 = this.num1;
+    num2 = this.num2;
+    let substract = num1 - num2;
+    return substract;
   }
 
   multiply(num1, num2) {
-    return num1 * num2;
+    num1 = this.num1;
+    num2 = this.num2;
+    let multiply = num1 * num2;
+    return multiply;
   }
 
   divide(num1, num2) {
-    return num1 / num2;
+    num1 = this.num1;
+    num2 = this.num2;
+    let divide = num1 / num2;
+    return divide;
   }
 }
 
