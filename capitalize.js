e0d15540ebad576a755903145079283cbab76e60
franclobo <fjbl2788@gmail.com>
2022-05-17 20:42:26 -0500

    Fix linters

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,3 @@
-const capitalize = str => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 module.exports = capitalize;
--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,3 @@
-const stringLength = str => {
-  return str.length <= 10 && str.length > 1 ? str.length : false;
-};
+const stringLength = (str) => str.length <= 10 && str.length > 1 ? str.length : false;
 
 module.exports = stringLength;
