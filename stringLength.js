b44fe831d3aef31cefce5abf25aba9e996faee66
franclobo <fjbl2788@gmail.com>
2022-05-17 20:47:14 -0500

    Fix linters

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,6 @@
 const stringLength = (str) => {
-  return str.length <= 10 && str.length > 1 ? str.length : false;
+  let count = str.length <= 10 && str.length > 1 ? str.length : false
+  return count;
 };
 
 module.exports = stringLength;
